39b99af2634e46607b7a9562aa207d1459f0aa9e
Hunter Rafuse <hunter_r_mazza@protonmail.com>
2023-04-16 00:15:44 +0300

    Update for Assignment 5 Part 2

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+case "$1" in
+    start)
+        echo "Starting simple server"
+        start-stop-daemon -S -n simpleserver -a /usr/bin/simpleserver
+        ;;
+    stop)
+        echo "Stopping simple server"
+        start-stop-daemon -K -n simpleserver
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+exit 0
