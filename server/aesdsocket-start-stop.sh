f9580b6c499a6a029e91ffa973e94be4785c7902
Hunter Rafuse <hunter_r_mazza@protonmail.com>
2023-04-16 00:38:22 +0300

    Update daemon script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,12 +2,12 @@
 
 case "$1" in
     start)
-        echo "Starting simple server"
-        start-stop-daemon -S -n simpleserver -a /usr/bin/simpleserver
+        echo "Starting simple aesdsocket server"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
-        echo "Stopping simple server"
-        start-stop-daemon -K -n simpleserver
+        echo "Stopping simple aesdsocket server"
+        start-stop-daemon -K -n aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"
